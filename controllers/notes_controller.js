98cef042b209cca5828dfb60f66bc8e5ab8a3982
dsl2055 <job2019tmm@gmail.com>
2022-08-08 22:08:10 -0400

    updated

--- controllers/notes_controller.js
+++ controllers/notes_controller.js
@@ -4,6 +4,14 @@ const Note = require("../models/notes.js");
 // const Author = require("../models/authors.js");
 const notesSeedData = require("../models/notes_seed");
 
+notes.get("/", async (req, res) => {
+  const notes = await Note.find();
+  res.render("index", {
+    notes: notes,
+    title: "Todo App Home Page",
+  });
+});
+
 notes.get("/data/seed", async (req, res) => {
   const seeded = await Note.insertMany(notesSeedData);
   console.log(seeded);
--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,19 @@
 const React = require("react");
 const Default = require("./layouts/Default");
 
-function Index() {
+function Index({ notes, title }) {
+  console.log(notes);
+  const notesRendered = notes.map((note) => (
+    <li key={note._id}>
+      <h3>{note.title}</h3>
+      <h4>{note.noteBody}</h4>
+      <h5>{`Created on: ${note.createdAt}`}</h5>
+    </li>
+  ));
   return (
     <Default title="temp">
-      <div>Index</div>
+      <h2>{title}</h2>
+      <ul>{notesRendered}</ul>
     </Default>
   );
 }
